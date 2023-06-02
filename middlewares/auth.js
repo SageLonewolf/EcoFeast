9a239c47a8fa7f99b928f423ae408fb78c1e2049
Manas Kumar <manas.dps@rediffmail.com>
2023-06-02 23:20:30 +0530

    updated middlware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,7 @@ const auth = async (req, res, next) => {
     if (!verified)
       return res
         .status(401)
-        .json({ msg: "Token verification failed, authorization denied." });
+        .json({ msg: "Token verification failed , authorization denied." });
 
     req.user = verified.id;
     req.token = token;
